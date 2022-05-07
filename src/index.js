32f98c303db730d930481f60a18ec50349a7fe6b
saransh-283 <saranshmm@gmail.com>
2022-05-07 14:43:34 +0530

    Tailwind Setup Done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import FirebaseContext from "./context/firebase";
 import { firebase, FieldValue } from "./lib/firebase";
+import "./styles/app.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,22 +1,25 @@
-import { useContext, useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import FirebaseContext from "../context/firebase"
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+import "../styles/app.css";
 export default function Login() {
-    const history = useNavigate()
-    const { firebase } = useContext(FirebaseContext)
+  const history = useNavigate();
+  const { firebase } = useContext(FirebaseContext);
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [error, setError] = useState('')
-    const isInvalid = password === '' || email === ''
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || email === "";
 
-    const handleLogin = () => {
+  const handleLogin = () => {};
 
-    }
-
-    useEffect(() => {
-        document.title = "Login - Instagram"
-    })
-    return (<p>I am the login page</p>)
+  useEffect(() => {
+    document.title = "Login - Instagram";
+  });
+  return (
+    <div className="container flex mx-auto max-w-screen-mid items-center h-screen">
+      <p>No Idea</p>
+    </div>
+  );
 }
